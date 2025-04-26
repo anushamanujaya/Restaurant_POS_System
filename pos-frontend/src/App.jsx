14dec34b1bce1bdc45e53ff8d3d66b53e895319d
anushamanujaya <anushmanuja@gmail.com>
2025-04-26 23:23:43 +0530

    h

--- pos-frontend/src/App.jsx
+++ pos-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home, Auth, Orders } from "./pages";
+import Headers from "./components/shared/Headers.jsx";
 
 function App() {
   return (
