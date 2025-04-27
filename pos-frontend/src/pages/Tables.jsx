938b54cb3bee79edcea81c00c5c93f07d27164a0
anushamanujaya <anushmanuja@gmail.com>
2025-04-28 00:59:14 +0530

    tables

---
+++ pos-frontend/src/pages/Tables.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import BottomNav from '../components/shared/BottomNav'
+import BackButton from '../components/shared/BackButton'
+import TableCard from '../components/tables/TableCard'
+import { tables } from '../constants'
+
+const Tables = () => {
+
+    const [status, setStatus] = useState('all');
+
+  return (
+    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden'>
+        <div className='flex items-center justify-between  px-10 py-4 mt-2'>
+            <div className='flex items-center gap-4'>
+                <BackButton />
+                <h1 className='text-[#f5f5f5] text-2 xl font-bold tracking-wider'>
+                    Tables
+                </h1>
+            </div>
+            <div className='flex items-center justify-around gap-4'>
+                <button onClick={() => setStatus('all')} className={`text-[#ababab] text-lg ${status === "all" && "bg-[#383838]  rounded-lg px-5 py-2"} rounded-lg px-5 py-2 font-semibold`}>
+                    All
+                </button>
+
+                <button onClick={() => setStatus('Booked')} className={`text-[#ababab] text-lg ${status === "Booked" && "bg-[#383838]  rounded-lg px-5 py-2"} rounded-lg px-5 py-2 font-semibold`}>
+                    Booked
+                </button>
+            </div>
+        </div>
+        <div className='p-10 flex flex-wrap gap-6 px-16 py-4 overflow-y-scroll scrollbar-hide h-[calc(100vh-5rem-5rem)] '>
+            {
+                tables.map((table) => {
+                    return (
+                        <TableCard key={table.id} name={table.name} status={table.status} initial={table.initial} />
+                    )
+                })
+            }
+            
+        </div>
+
+        <BottomNav />
+    </section>
+  )
+}
+
+export default Tables
