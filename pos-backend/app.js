0def8fcf899af129f86e7dbfe625ca12760a9c36
anushamanujaya <anushmanuja@gmail.com>
2025-05-09 22:16:50 +0530

    database setup, folder structure setup

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -1,4 +1,15 @@
-const express = require(:express);
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
 
 const PORT = process.env.PORT;
+connectDB();
+
+app.get("/", (req,res) => {
+    res.json({message : "hello anush"});
+})
+
+app.listen(PORT, () => {
+    console.log(`server is listing on port ${PORT}`);
+})
